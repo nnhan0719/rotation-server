c66305643390faf301f1f90cee539c88ccff2993
Nguyen Nhan <nnhan0719@gmail.com>
2024-03-01 22:40:27 +0700

    update

--- app.js
+++ app.js
@@ -1,13 +1,20 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const path = require("path");
+
+const defaultHtml = (res) => {
+  return res.sendFile(path.join(__dirname, "/html/index.html"));
+};
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.get("/dark-blue-f8", (req, res) => {
-  return res.redirect(307,"https://zaksrealm.net");
+  return defaultHtml(res);
+
+  return res.redirect(307, "https://zaksrealm.net");
 });
 
 app.listen(port, () => {
